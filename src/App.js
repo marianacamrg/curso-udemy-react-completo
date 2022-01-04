fb1282a6a98213f0b437ed96cda136c31314f0c6
Mariana <marianacamrg@gmail.com>
2022-01-04 14:18:24 -0300

    Aula 20. Criando Componentes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import Comentario from './components/Comentario';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Meu Projeto</h1>
+      <Comentario />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Comentario.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Comentario.css';
+
+const Comentario = () => (
+    <div className="Comentario">
+      <div>João</div>
+      <div>Olá, tudo bem?</div>
+    </div>
+);
+
+export default Comentario;
