9eaa95c6c85af1d1f8c59cbf40851cb556ae940a
Mariana <marianacamrg@gmail.com>
2022-01-06 16:30:49 -0300

    Formatando a data com o date-fns

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import './App.css';
 
+import './App.css';
 import Comentario from './components/Comentario';
 
 class App extends Component {
--- src/components/Comentario.js
+++ src/components/Comentario.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { formatRelative } from 'date-fns';
+import { ptBR } from 'date-fns/locale'
+
 import './Comentario.css';
 import imagemUsuario from './usuario.png';
 
@@ -9,7 +12,7 @@ const Comentario = (props) => (
       <h2 class="nome">{props.nome}</h2>
       <p class="email">{props.email}</p>
       <p class="mensagem">{props.children}</p>
-      <p class="data">{props.data.toString()}</p>
+      <p class="data">{formatRelative(props.data, new Date(), {locale: ptBR })}</p>
       <button class="button" onClick={props.onRemove}>&times;</button>
     </div>
 
