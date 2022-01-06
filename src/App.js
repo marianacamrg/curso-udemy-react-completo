089074afd0e6ef0b1ea48424a18c8008b1cd2e2d
Mariana <marianacamrg@gmail.com>
2022-01-06 14:13:25 -0300

    Aula 21. Props

--- src/App.js
+++ src/App.js
@@ -7,7 +7,15 @@ function App() {
   return (
     <div className="App">
       <h1>Meu Projeto</h1>
-      <Comentario />
+      <Comentario nome="Mariana" email="marianacamrg@gmail.com" data={new Date(1999, 4, 14)}>
+        Olá, tudo bem?
+      </Comentario>
+      <Comentario nome="Natalia" email="natalia@gmail.com" data={new Date(1999, 1, 17)}>
+        Olá, tudo certo e com você?
+      </Comentario>
+      <Comentario nome="Carlos" email="carlos@gmail.com" data={new Date(1999, 12, 20)}>
+        Olá, tudo certo por aqui
+      </Comentario>
     </div>
   );
 }
--- src/components/Comentario.js
+++ src/components/Comentario.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Comentario.css';
 
-const Comentario = () => (
+const Comentario = (props) => (
     <div className="Comentario">
-      <div>João</div>
-      <div>Olá, tudo bem?</div>
+      <h2>{props.nome}</h2>
+      <p>{props.email}</p>
+      <p>{props.data.toString()}</p>
+      <p>{props.children}</p>
     </div>
 );
 
